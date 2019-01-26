b24c365cf19154eafd6ac528b72f63cae241d1d2
mesona <kylemesona@gmail.com>
2019-01-25 16:00:47 -0800

    mid day update

--- active_record_lite/lib/01_sql_object.rb
+++ active_record_lite/lib/01_sql_object.rb
@@ -1,22 +1,51 @@
 require_relative 'db_connection'
 require 'active_support/inflector'
+# require 'byebug'
 # NB: the attr_accessor we wrote in phase 0 is NOT used in the rest
 # of this project. It was only a warm up.
 
 class SQLObject
   def self.columns
-    # ...
+    
+    table = self.table_name
+    @data ||= []
+    if @data == []
+      @data = DBConnection.execute2(<<-SQL)
+        SELECT
+          *
+        FROM
+          "#{table}"
+        SQL
+    end
+
+    @data.first.map {|el| el.to_sym}
   end
 
   def self.finalize!
+    self.columns.each do |column|
+
+      define_method("#{column}=") do |val|
+        self.attributes[column] = val
+      end
+
+      define_method(column) do
+        self.attributes[column]
+      end
+
+    end
   end
 
   def self.table_name=(table_name)
-    # ...
+    @table_name = table_name
   end
 
   def self.table_name
-    # ...
+    if @table_name == nil
+      @table_name = "#{self}".tableize
+    end
+
+    @table_name
+
   end
 
   def self.all
@@ -32,11 +61,18 @@ class SQLObject
   end
 
   def initialize(params = {})
-    # ...
+    params.each do |key, val|
+      if self.class.columns.include?(key.to_sym)
+        k = key.to_sym
+        self.send("#{k}=", val)
+      else
+        raise "unknown attribute '#{key}'"
+      end
+    end
   end
 
   def attributes
-    # ...
+    @attributes ||= {}
   end
 
   def attribute_values
